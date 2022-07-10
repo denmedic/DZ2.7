369ce77db515610836f170d9eab007b0f0b7f9e5
denisskarednyh <deniska2019@yandex.ru>
2022-07-10 08:50:11 +0300

    1

---
+++ src/test/java/com/example/dz2_7/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dz2_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
